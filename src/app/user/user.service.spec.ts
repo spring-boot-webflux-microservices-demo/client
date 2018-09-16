0cd4d1d89b6ab9da7b51fc8a4c74f38bed830d54
VK <viktoras.lipinskis@gmail.com>
2018-09-16 08:40:02 +0300

    written test for getAll users

--- src/app/user/user.service.spec.ts
+++ src/app/user/user.service.spec.ts
@@ -1,15 +1,39 @@
-import { TestBed, inject } from '@angular/core/testing';
+import {TestBed, inject} from '@angular/core/testing';
 
-import { UserService } from './user.service';
+import {HttpClientModule, HttpRequest} from '@angular/common/http';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {UserService} from './user.service';
 
-describe('UserService', () => {
+describe(`FakeHttpClientResponses`, () => {
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [UserService]
+      imports: [
+        HttpClientModule,
+        HttpClientTestingModule
+      ]
     });
   });
 
-  it('should be created', inject([UserService], (service: UserService) => {
-    expect(service).toBeTruthy();
-  }));
+  fit(`should return all users`,
+    inject(
+      [HttpTestingController, UserService],
+      (httpMock: HttpTestingController, service: UserService) => {
+        const mockResponse = [{
+          id: 'id',
+          firstName: 'firstName',
+          lastName: 'lastName',
+          age: '55'
+        }];
+        const mockUrl = '//localhost:8081/api1/findAllUsers';
+
+        service.getAll().subscribe(data => {
+          expect(data).toEqual(mockResponse);
+        });
+
+        const mockRequest = httpMock.expectOne((req: HttpRequest<any>) => req.url === mockUrl);
+        expect(mockRequest.request.method).toBe('GET');
+        mockRequest.flush(mockResponse);
+      })
+  );
 });
