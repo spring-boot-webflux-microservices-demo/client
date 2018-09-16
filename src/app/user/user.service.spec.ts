291efb267ce10b7213c4135c2506e1fb07424fe5
VK <viktoras.lipinskis@gmail.com>
2018-09-16 22:46:28 +0300

    written test for post user

--- src/app/user/user.service.spec.ts
+++ src/app/user/user.service.spec.ts
@@ -1,8 +1,9 @@
 import {TestBed, inject} from '@angular/core/testing';
 
-import {HttpClientModule, HttpRequest} from '@angular/common/http';
+import {HttpClientModule, HttpParams, HttpRequest} from '@angular/common/http';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import {UserService} from './user.service';
+import {User} from './user';
 
 describe(`FakeHttpClientResponses`, () => {
 
@@ -15,7 +16,7 @@ describe(`FakeHttpClientResponses`, () => {
     });
   });
 
-  fit(`should return all users`,
+  it('should return all users',
     inject(
       [HttpTestingController, UserService],
       (httpMock: HttpTestingController, service: UserService) => {
@@ -36,4 +37,28 @@ describe(`FakeHttpClientResponses`, () => {
         mockRequest.flush(mockResponse);
       })
   );
+
+  it('should save user', () => {
+    const userService = TestBed.get(UserService);
+    const http = TestBed.get(HttpTestingController);
+    let expectedResponse = null;
+
+    userService.saveUser(new UserMock()).subscribe(data => {
+      expectedResponse = data;
+    });
+
+    const responseMock = {id: 'savedId', firstName: 'savedFirstName', lastName: 'savedLastName', age: 1};
+    http.expectOne({
+      url: '//localhost:8081/api1/saveUser',
+      method: 'POST'
+    }).flush(responseMock);
+
+    expect(expectedResponse).toEqual(responseMock);
+  });
+
+  class UserMock implements User {
+    constructor();
+    constructor(public id: string, public firstName: string, public lastName: string, public age: number) {
+    }
+  }
 });
