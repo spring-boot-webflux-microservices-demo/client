2a785fa408a57274518bc034bcacc464857f88f3
VK <viktoras.lipinskis@gmail.com>
2018-10-03 07:10:13 +0300

    refactoring

--- src/app/gadget/gadget.service.spec.ts
+++ src/app/gadget/gadget.service.spec.ts
@@ -5,24 +5,26 @@ import {HttpClientModule, HttpRequest} from '@angular/common/http';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 describe('GadgetService', () => {
+  let gadgetService;
+  let http;
+  let response;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule,
         HttpClientTestingModule]
     });
+
+    gadgetService = TestBed.get(GadgetService);
+    http = TestBed.get(HttpTestingController);
+    response = null;
   });
 
   it('should return all gadgets', () => {
-    const gadgetService = TestBed.get(GadgetService);
-    const http = TestBed.get(HttpTestingController);
-    let response = null;
-
     gadgetService.getAll().subscribe(data => response = data);
-
     const mockRequest = http.expectOne((req: HttpRequest<any>) => req.url === '//localhost:8082/api2/findAllGadgets')
     mockRequest.flush({id: 'mockedId'});
-
-    expect({id: 'mockedId'}).toEqual(response);
+    expect(response).toEqual({id: 'mockedId'});
     expect(mockRequest.request.method).toBe('GET');
   });
 });
