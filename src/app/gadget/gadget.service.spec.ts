8d7d0f99e12a097c163242dc18eb3a6b4a5ab1ae
VK <viktoras.lipinskis@gmail.com>
2018-10-03 07:04:55 +0300

    written test for gadget service

--- src/app/gadget/gadget.service.spec.ts
+++ src/app/gadget/gadget.service.spec.ts
@@ -1,15 +1,28 @@
-import { TestBed, inject } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { GadgetService } from './gadget.service';
+import {GadgetService} from './gadget.service';
+import {HttpClientModule, HttpRequest} from '@angular/common/http';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 describe('GadgetService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [GadgetService]
+      imports: [HttpClientModule,
+        HttpClientTestingModule]
     });
   });
 
-  it('should be created', inject([GadgetService], (service: GadgetService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should return all gadgets', () => {
+    const gadgetService = TestBed.get(GadgetService);
+    const http = TestBed.get(HttpTestingController);
+    let response = null;
+
+    gadgetService.getAll().subscribe(data => response = data);
+
+    const mockRequest = http.expectOne((req: HttpRequest<any>) => req.url === '//localhost:8082/api2/findAllGadgets')
+    mockRequest.flush({id: 'mockedId'});
+
+    expect({id: 'mockedId'}).toEqual(response);
+    expect(mockRequest.request.method).toBe('GET');
+  });
 });
