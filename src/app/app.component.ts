c5d92e8e05c7de7a30029e44905ef3ac04a92aae
VK <viktoras.lipinskis@gmail.com>
2018-11-11 07:36:18 +0200

    fixed bugs / refactored routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import {Component} from '@angular/core';
-import {MatTabChangeEvent} from '@angular/material';
-import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,25 +7,9 @@ import {Router} from '@angular/router';
 })
 
 export class AppComponent {
-  userTitle = 'User';
-  gadgetTitle = 'Gadget';
   title = 'client';
-  userDescription = 'User content';
-  gadgetDescription = 'Gadget content';
-
-  constructor(private router: Router) {
-  }
-
-  onTabClick(event: MatTabChangeEvent) {
-    const tabText = event.tab.textLabel;
-
-    if (tabText === this.userTitle) {
-      this.router.navigateByUrl('/users');
-    }
-
-    if (tabText === this.gadgetTitle) {
-      this.router.navigateByUrl('/gadgets');
-    }
-
-  }
+  navLinks = [
+    {path: 'users', label: 'Users'},
+    {path: 'gadgets', label: 'Gadgets'}
+  ];
 }
